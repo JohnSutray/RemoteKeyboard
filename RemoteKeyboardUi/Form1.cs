30105a0fef7adf1e74d737af0f5b4dc352d52acf
andrey zobkov <andrei.zabkou@coxautoinc.com>
2022-06-21 22:31:02 +0300

    initial

---
+++ RemoteKeyboardUi/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using com.jarvisniu.utils;
+using SocketIOClient;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace RemoteKeyboardUi {
+
+  [PermissionSet(SecurityAction.Demand, Name = "SkipVerification")]
+  public partial class Form1 : Form {
+    private SocketIO socketIo = new SocketIO("http://3.73.158.78:3000");
+    private KeyListener _listener = new KeyListener();
+    private InputSimulator _simulator = new InputSimulator();
+    
+    public Form1() {
+      InitializeComponent();
+
+      _listener.onPress("A", () => {
+        textBox1.Text += "A";
+        _simulator.Keyboard.KeyPress(VirtualKeyCode.VK_B);
+      });
+
+      socketIo.OnAny((type, response) => {
+          try {
+            Console.WriteLine(
+              type
+            );
+
+            Console.WriteLine(response.GetValue(1));
+          }
+          catch (Exception e) {
+            Console.WriteLine(e);
+            throw;
+          }
+        }
+      );
+
+      socketIo.OnError += (sender, s) => { Console.WriteLine(s); };
+
+      // socketIo.ConnectAsync().GetAwaiter().GetResult();
+      //
+      // socketIo.EmitAsync("receiver", new { id = "1" }).GetAwaiter().GetResult();
+      //
+      // socketIo.EmitAsync("keyboard", new { id = "1", value = new { a = 1 } }).GetAwaiter().GetResult();
+
+      Console.ReadLine();
+    }
+  }
+}
