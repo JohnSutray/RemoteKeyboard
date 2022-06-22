3a86de86070cf60a041037cb53b051b7741ce6bb
andrey zobkov <andrei.zabkou@coxautoinc.com>
2022-06-22 15:12:53 +0300

    it works

--- RemoteKeyboardUi/Form1.Designer.cs
+++ RemoteKeyboardUi/Form1.Designer.cs
@@ -24,29 +24,111 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
-      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.RoomIdTextbox = new System.Windows.Forms.TextBox();
+      this.RoomIdLabel = new System.Windows.Forms.Label();
+      this.ModeGroupBox = new System.Windows.Forms.GroupBox();
+      this.SendModeRadioButton = new System.Windows.Forms.RadioButton();
+      this.ReceiveModeRadioButton = new System.Windows.Forms.RadioButton();
+      this.LogTextBox = new System.Windows.Forms.RichTextBox();
+      this.StopButton = new System.Windows.Forms.Button();
+      this.ModeGroupBox.SuspendLayout();
       this.SuspendLayout();
       // 
-      // textBox1
+      // RoomIdTextbox
       // 
-      this.textBox1.Location = new System.Drawing.Point(154, 197);
-      this.textBox1.Name = "textBox1";
-      this.textBox1.Size = new System.Drawing.Size(220, 22);
-      this.textBox1.TabIndex = 0;
+      this.RoomIdTextbox.Location = new System.Drawing.Point(13, 38);
+      this.RoomIdTextbox.Name = "RoomIdTextbox";
+      this.RoomIdTextbox.Size = new System.Drawing.Size(213, 22);
+      this.RoomIdTextbox.TabIndex = 0;
+      // 
+      // RoomIdLabel
+      // 
+      this.RoomIdLabel.Location = new System.Drawing.Point(12, 9);
+      this.RoomIdLabel.Name = "RoomIdLabel";
+      this.RoomIdLabel.Size = new System.Drawing.Size(214, 23);
+      this.RoomIdLabel.TabIndex = 1;
+      this.RoomIdLabel.Text = "Room ID";
+      // 
+      // ModeGroupBox
+      // 
+      this.ModeGroupBox.Controls.Add(this.SendModeRadioButton);
+      this.ModeGroupBox.Controls.Add(this.ReceiveModeRadioButton);
+      this.ModeGroupBox.Location = new System.Drawing.Point(13, 68);
+      this.ModeGroupBox.Name = "ModeGroupBox";
+      this.ModeGroupBox.Size = new System.Drawing.Size(119, 101);
+      this.ModeGroupBox.TabIndex = 2;
+      this.ModeGroupBox.TabStop = false;
+      this.ModeGroupBox.Text = "Mode";
+      // 
+      // SendModeRadioButton
+      // 
+      this.SendModeRadioButton.Location = new System.Drawing.Point(12, 66);
+      this.SendModeRadioButton.Name = "SendModeRadioButton";
+      this.SendModeRadioButton.Size = new System.Drawing.Size(103, 29);
+      this.SendModeRadioButton.TabIndex = 1;
+      this.SendModeRadioButton.TabStop = true;
+      this.SendModeRadioButton.Text = "Send";
+      this.SendModeRadioButton.UseVisualStyleBackColor = true;
+      this.SendModeRadioButton.CheckedChanged += new System.EventHandler(this.SendModeRadioButton_CheckedChanged);
+      // 
+      // ReceiveModeRadioButton
+      // 
+      this.ReceiveModeRadioButton.Location = new System.Drawing.Point(12, 18);
+      this.ReceiveModeRadioButton.Name = "ReceiveModeRadioButton";
+      this.ReceiveModeRadioButton.Size = new System.Drawing.Size(101, 28);
+      this.ReceiveModeRadioButton.TabIndex = 0;
+      this.ReceiveModeRadioButton.TabStop = true;
+      this.ReceiveModeRadioButton.Text = "Receive";
+      this.ReceiveModeRadioButton.UseVisualStyleBackColor = true;
+      this.ReceiveModeRadioButton.CheckedChanged += new System.EventHandler(this.ReceiveModeRadioButton_CheckedChanged);
+      // 
+      // LogTextBox
+      // 
+      this.LogTextBox.Location = new System.Drawing.Point(237, 12);
+      this.LogTextBox.Name = "LogTextBox";
+      this.LogTextBox.ReadOnly = true;
+      this.LogTextBox.Size = new System.Drawing.Size(274, 157);
+      this.LogTextBox.TabIndex = 3;
+      this.LogTextBox.Text = "";
+      // 
+      // StopButton
+      // 
+      this.StopButton.Location = new System.Drawing.Point(140, 77);
+      this.StopButton.Name = "StopButton";
+      this.StopButton.Size = new System.Drawing.Size(86, 92);
+      this.StopButton.TabIndex = 4;
+      this.StopButton.Text = "Stop";
+      this.StopButton.UseVisualStyleBackColor = true;
+      this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Controls.Add(this.textBox1);
+      this.ClientSize = new System.Drawing.Size(523, 181);
+      this.Controls.Add(this.StopButton);
+      this.Controls.Add(this.LogTextBox);
+      this.Controls.Add(this.ModeGroupBox);
+      this.Controls.Add(this.RoomIdLabel);
+      this.Controls.Add(this.RoomIdTextbox);
       this.Name = "Form1";
       this.Text = "Form1";
+      this.ModeGroupBox.ResumeLayout(false);
       this.ResumeLayout(false);
       this.PerformLayout();
     }
 
-    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button StopButton;
+
+    private System.Windows.Forms.RichTextBox LogTextBox;
+
+    private System.Windows.Forms.GroupBox ModeGroupBox;
+    private System.Windows.Forms.RadioButton ReceiveModeRadioButton;
+    private System.Windows.Forms.RadioButton SendModeRadioButton;
+
+    private System.Windows.Forms.Label RoomIdLabel;
+
+    private System.Windows.Forms.TextBox RoomIdTextbox;
 
     #endregion
   }
--- RemoteKeyboardUi/Form1.cs
+++ RemoteKeyboardUi/Form1.cs
@@ -1,62 +1,210 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Security.Permissions;
+using System.Text.Json;
 using System.Windows.Forms;
-using com.jarvisniu.utils;
 using SocketIOClient;
 using WindowsInput;
 using WindowsInput.Native;
+using Timer = System.Timers.Timer;
 
 namespace RemoteKeyboardUi {
+  public static class Logger {
+    public static void Log(string message) => File.AppendAllText("./log.txt", $"{message}\n");
+    
+    public static void TryWithLog(Action action) {
+      try {
+        action();
+      }
+      catch (Exception e) {
+        Log(e.ToString());
+      }
+    }
+  }
+  
+  public class KeyboardStateEmulator {
+    private Timer _timer = new Timer(50);
+    private Dictionary<string, bool> _keyState = KeyboardMap.ITEMS.Keys.ToDictionary(k => k, k => false);
+    private InputSimulator _simulator = new InputSimulator();
+    private List<VirtualKeyCode> _dirtyKeys = new List<VirtualKeyCode>();
+    private List<VirtualKeyCode> _keydownKeys = new List<VirtualKeyCode>();
+    private List<VirtualKeyCode> _keyupKeys = new List<VirtualKeyCode>();
+    private Dictionary<string, bool> _lastPatch = new Dictionary<string, bool>();
 
+    public KeyboardStateEmulator() {
+      _timer.Elapsed += (sender, args) => EmulationTick();
+    }
+
+    public void PatchState(Dictionary<string, bool> patch) {
+      _lastPatch = patch;
+    }
+
+    public void Start() {
+      _timer.Start();
+    }
+    
+    public void Stop() {
+      _timer.Stop();
+    }
+
+    private void EmulationTick() {
+      Logger.TryWithLog(() => {
+        if (_lastPatch == null) {
+          return;
+        }
+        
+        foreach (var patch in _lastPatch) {
+          if (patch.Value && !_keyState[patch.Key]) {
+            _keydownKeys.Add(KeyboardMap.TranslateKey(patch.Key));
+          }
+
+          if (!patch.Value && _keyState[patch.Key]) {
+            _keyupKeys.Add(KeyboardMap.TranslateKey(patch.Key));
+          }
+          
+          _keyState[patch.Key] = patch.Value;
+        }
+      
+        foreach (var keyState in _keyState) {
+          if (keyState.Value) {
+            _dirtyKeys.Add(KeyboardMap.TranslateKey(keyState.Key));
+          }
+        }
+        
+        Logger.Log(_dirtyKeys.Count.ToString());
+
+        if (_dirtyKeys.Any()) {
+          _simulator.Keyboard.KeyPress(_dirtyKeys.ToArray());
+        }
+        
+        _dirtyKeys.Clear();
+        _keydownKeys.Clear();
+        _keyupKeys.Clear();
+        _lastPatch = null;
+      });
+    }
+  }
+  
   [PermissionSet(SecurityAction.Demand, Name = "SkipVerification")]
   public partial class Form1 : Form {
-    private SocketIO socketIo = new SocketIO("http://3.73.158.78:3000");
-    private KeyListener _listener = new KeyListener();
-    private InputSimulator _simulator = new InputSimulator();
+    private SocketIO _socket = new SocketIO("http://18.197.33.127:3000");
+    private KeyListener _sendModeListener = new KeyListener();
+    private KeyboardStateEmulator _keyboardStateEmulator = new KeyboardStateEmulator();
 
-    private void initializeHost(string id) {
-      _simulator.Keyboard.KeyPress(VirtualKeyCode.VK_B);
+    private bool InReceiveMode => ReceiveModeRadioButton.Checked;
+    private bool InSendMode => SendModeRadioButton.Checked;
+    private string CurrentRoomId => RoomIdTextbox.Text;
+
+    private void InitializeCheckers() {
+      _sendModeListener.DirtyKeysChanged += SendKeyboard;
+    }
+
+    private void SendKeyboard(List<string> dirtyKeys) {
+      if (!InSendMode || dirtyKeys.Count == 0) {
+        return;
+      }
+
+      var keys = new Dictionary<string, bool>();
+
+      foreach (var dirtyItem in dirtyKeys) {
+        keys[dirtyItem] = _sendModeListener.CurrentSingleKeyStates[dirtyItem];
+      }
+
+      _socket.EmitAsync("keyboard", new {
+        id = RoomIdTextbox.Text,
+        value = keys
+      });
+    }
+
+    private void CheckRoomIdValidity() {
+      ModeGroupBox.Enabled = RoomIdTextbox.Text.Length > 2;
     }
 
     public Form1() {
       InitializeComponent();
+      InitializeCheckers();
+      InitializeConnection();
+      InitializeReceiver();
+      EnableSetupControls();
+      CheckRoomIdValidity();
+      
+      RoomIdTextbox.TextChanged += (sender, args) => CheckRoomIdValidity();
+    }
 
-      foreach (var itemsKey in KeyboardMap.ITEMS.Keys) {
-        _listener.onPress(itemsKey, () => {
-          textBox1.Text += "A";
-        });
+    private void ReceiveKeys(SocketIOResponse response) {
+      var keysState = response.GetValue(1).Deserialize<Dictionary<string, bool>>();
 
-        _listener.onRelease(itemsKey, () => {
+      _keyboardStateEmulator.PatchState(keysState);
+    }
 
-        });
-      }
-
-      socketIo.OnAny((type, response) => {
-          try {
-            Console.WriteLine(
-              type
-            );
-
-            Console.WriteLine(response.GetValue(1));
+    private void InitializeReceiver() {
+      _socket.OnAny((message, response) => {
+          if (LogTextBox.InvokeRequired) {
+            LogTextBox.Invoke(new Action(() => ReceiveKeys(response)));
           }
-          catch (Exception e) {
-            Console.WriteLine(e);
-            throw;
-          }
+
+          // LogTextBox.Text += string.Join(", ", response.GetValue(1).Deserialize<Dictionary<string, string>>().Keys) + "\n";
         }
       );
+    }
 
-      socketIo.OnError += (sender, s) => { Console.WriteLine(s); };
+    private void InitializeConnection() {
+      _socket.ConnectAsync();
+    }
 
-      // socketIo.ConnectAsync().GetAwaiter().GetResult();
-      //
-      // socketIo.EmitAsync("receiver", new { id = "1" }).GetAwaiter().GetResult();
-      //
-      // socketIo.EmitAsync("keyboard", new { id = "1", value = new { a = 1 } }).GetAwaiter().GetResult();
+    private void ReceiveModeRadioButton_CheckedChanged(object sender, EventArgs e) {
+      if (InReceiveMode) {
+        StartReceiver();
+      }
+      else {
+        StopReceiver();
+      }
+      
+      DisableSetupControls();
+    }
 
-      Console.ReadLine();
+    private void StartReceiver() {
+      _socket.EmitAsync("receiver", new { id = CurrentRoomId });
+      _keyboardStateEmulator.Start();
+    }
+
+    private void StopReceiver() {
+      _socket.EmitAsync("receiver-stop", new { id = CurrentRoomId });
+      _keyboardStateEmulator.Stop();
+    }
+
+    private void SendModeRadioButton_CheckedChanged(object sender, EventArgs e) {
+      if (SendModeRadioButton.Checked) {
+        _sendModeListener.Start();
+      }
+      else {
+        _sendModeListener.Stop();
+      }
+      
+      DisableSetupControls();
+    }
+
+    private void DisableSetupControls() {
+      StopButton.Enabled = true;
+      RoomIdTextbox.Enabled = false;
+      ModeGroupBox.Enabled = false;
+    }
+
+    private void StopButton_Click(object sender, EventArgs e) {
+      if (InReceiveMode) {
+        StopReceiver();
+      }
+      
+      EnableSetupControls();
+    }
+
+    private void EnableSetupControls() {
+      StopButton.Enabled = false;
+      RoomIdTextbox.Enabled = true;
+      ModeGroupBox.Enabled = true;
     }
   }
+  
 }
